460c43dcb0c79195b68b12d6f5a46b5e4c33ee04
Clara Ruano Frugoli <cruano@fi.uba.ar>
2024-05-16 12:14:20 -0300

    Update client.go

--- client.go
+++ client.go
@@ -1,117 +1,137 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	"log"
-	"net"
+	"os"
 
-	rl "github.com/gen2brain/raylib-go/raylib"
+	// "log"
+	"net"
 )
 
-func startClient() {
-	conn, err := net.Dial("tcp", "localhost:8080")
+func welcomeUser(playerID string) {
+	fmt.Println("Connected to server with ID:", playerID)
+}
+
+func dial(serverAddress string) net.Conn {
+	connection, err := net.Dial("tcp", serverAddress)
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
-		return
+		os.Exit(1)
 	}
-	defer conn.Close()
-
-	decoder := json.NewDecoder(conn)
-	var clientID string
-	err = decoder.Decode(&clientID)
+	return connection
+}
+func receiveMessageFromServer(conn net.Conn) (*Game, error) {
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("error al leer del servidor: %s", err)
 	}
 
-	fmt.Print("Enter message: ")
-	message := Message{
-		Action: "join",
+	decodedGame, err := decodeGame(buffer[:n])
+	if err != nil {
+		return nil, fmt.Errorf("error al decodificar el juego del servidor: %s", err)
 	}
 
-	encoder := json.NewEncoder(conn)
-	err = encoder.Encode(message)
+	return decodedGame, nil
+}
+
+func updateGame(conn net.Conn, game *Game) {
+	go func() {
+		fmt.Println("Updating game...")
+
+		updatedGame, err := receiveMessageFromServer(conn)
+		if err != nil {
+			fmt.Println("Error al recibir el juego actualizado:", err)
+			return
+		}
+
+		*game = *updatedGame
+	}()
+}
+
+func sendMessage(ClientMessage ClientMessage, connection net.Conn) {
+	encoded, err := encodeClientMessage(ClientMessage)
 	if err != nil {
 		fmt.Println("Error encoding message:", err)
 		return
 	}
-
-	rl.InitWindow(800, 800, "Bomberman Go!")
-	defer rl.CloseWindow()
-
-	rl.SetTargetFPS(60)
-	var game Game
-
-	go func() {
-		decoder := json.NewDecoder(conn)
-		for {
-			err := decoder.Decode(&game)
-			if err != nil {
-				fmt.Println("Error decoding game object:", err)
-				return
-			}
-		}
-	}()
-
-	for !rl.WindowShouldClose() {
-		drawGame(game)
-		move := handleInput()
-
-		if move == "bomb" {
-			message := Message{
-				Action:   "bomb",
-				Data:     nil,
-				PlayerID: clientID,
-			}
-
-			err = encoder.Encode(message)
-			if err != nil {
-				fmt.Println("Error encoding message:", err)
-				return
-			}
-		} else if move == "none" {
-			message := Message{
-				Action:   "update",
-				Data:     nil,
-				PlayerID: clientID,
-			}
-
-			err = encoder.Encode(message)
-			if err != nil {
-				fmt.Println("Error encoding message:", err)
-				return
-			}
-		} else {
-			moveData := map[string]interface{}{
-				"direction": move,
-			}
-
-			message := Message{
-				Action:   "move",
-				Data:     moveData,
-				PlayerID: clientID,
-			}
-
-			err = encoder.Encode(message)
-			if err != nil {
-				fmt.Println("Error encoding message:", err)
-				return
-			}
-		}
-
-		if rl.WindowShouldClose() {
-			message := Message{
-				Action:   "leave",
-				Data:     nil,
-				PlayerID: clientID,
-			}
-
-			err = encoder.Encode(message)
-			if err != nil {
-				fmt.Println("Error encoding message:", err)
-				return
-			}
-		}
-
+	_, err = connection.Write(encoded)
+	if err != nil {
+		fmt.Println("Error al enviar el mensaje:", err)
+		return
 	}
+
+	fmt.Println("Mensaje enviado exitosamente.")
+
+}
+
+func sendMove(move string, connection net.Conn, playerID string) {
+	ClientMessage := ClientMessage{Action: "move", Data: move, ID: playerID}
+	sendMessage(ClientMessage, connection)
+}
+
+func sendBomb(connection net.Conn, playerID string) {
+	ClientMessage := ClientMessage{Action: "bomb", Data: nil, ID: playerID}
+	sendMessage(ClientMessage, connection)
+}
+
+func sendInput(input string, connection net.Conn, playerID string) {
+	if input == "bomb" {
+		sendBomb(connection, playerID)
+	} else {
+		sendMove(input, connection, playerID)
+	}
+}
+
+func askForUpdates(connection net.Conn, playerID string) {
+	ClientMessage := ClientMessage{Action: "update", Data: nil, ID: playerID}
+	sendMessage(ClientMessage, connection)
+}
+
+func sendMessages(connection net.Conn, playerID string) {
+	input := handleInput()
+	if input != "none" {
+		sendInput(input, connection, playerID)
+	} else {
+		askForUpdates(connection, playerID)
+	}
+}
+
+func sendLeaveMessage(connection net.Conn, playerID string) {
+	ClientMessage := ClientMessage{Action: "leave", Data: nil, ID: playerID}
+	sendMessage(ClientMessage, connection)
+}
+
+func startClient() {
+	connection := dial(SERVER_ADDRESS)
+
+	defer connection.Close()
+
+	playerID := receiveId(connection)
+	welcomeUser(playerID)
+
+	initWindow()
+
+	var game Game
+	updateGame(connection, &game)
+
+	for !WindowShouldClose() {
+		//drawGame(game)
+		drawGame2()
+		sendMessages(connection, playerID)
+		if WindowShouldClose() {
+			sendLeaveMessage(connection, playerID)
+		}
+	}
+}
+
+func receiveId(conn net.Conn) string {
+	buffer := make([]byte, 37)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from server:", err)
+		return ""
+	}
+	return string(buffer[:n])
 }
