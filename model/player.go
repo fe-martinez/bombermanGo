066b7e1fe8f7cf837e84389f21f028670b65d60f
ianmku~ <ianmikaursino@gmail.com~>
2024-05-30 22:11:45 -0300

    add player direction and movement

--- model/player.go
+++ model/player.go
@@ -9,14 +9,24 @@ type Player struct {
 	Lives    int8
 	Bombs    int8
 	PowerUps []PowerUp
+	Speed 	 Speed
+	Direction string
+}
+
+type Speed struct {
+	X 		float32
+	Y		float32
 }
 
 func NewPlayer(id string, position *Position) *Player {
 	return &Player{
-		ID:       id,
-		Position: position,
-		Lives:    PLAYER_LIVES,
-		Bombs:    PLAYER_BOMBS,
-		PowerUps: []PowerUp{},
+		ID:        id,
+		Position:  position,
+		Lives:     PLAYER_LIVES,
+		Bombs:     PLAYER_BOMBS,
+		PowerUps:  []PowerUp{},
+		Speed:	   Speed{X: 0.0, Y: 0.0},
+		Direction: "up",
 	}
 }
+
--- server/gameLogic.go
+++ server/gameLogic.go
@@ -9,10 +9,32 @@ import (
 func handlePlayerAction(msg utils.ClientMessage, game *model.Game) {
 	switch msg.Action {
 	case utils.ActionMove:
-		fmt.Println("Received: ", msg.Data)
+		direction := msg.Data.(string)  
+		fmt.Println("Received: ", direction)
+		movePlayer(game.Players[msg.ID], direction)
 	case utils.ActionBomb:
 		fmt.Println("Received: ", msg.Action)
 	default:
 		fmt.Println("Action unknown")
+	}
+}
+
+func movePlayer(player *model.Player, direction string) {
+	switch direction {
+	case "up":
+		player.Speed.Y = -0.1
+		player.Position.Y += player.Speed.Y
+	case "right":
+		player.Speed.X = 0.1
+		player.Position.X += player.Speed.X
+	case "down":
+		player.Speed.Y = 0.1
+		player.Position.Y += player.Speed.Y
+	case "left":
+		player.Speed.X = -0.1
+		player.Position.X += player.Speed.X
+	
+	default:
+		player.Speed.X = 0
 	}
 }
