2e88b54706b63c8b632dca39e331ff01ca72b78d
claram97 <cruano@fi.uba.ar>
2024-06-06 14:25:54 -0300

    creo constante de explode time de bombas

--- model/bomb.go
+++ model/bomb.go
@@ -2,6 +2,8 @@ package model
 
 import "time"
 
+const EXPLODE_TIME = 3
+
 type Bomb struct {
 	Position    Position
 	Alcance     int8
@@ -15,7 +17,7 @@ func NewBomb(x, y float32, alcance int8, owner Player) *Bomb {
 		Position:    Position{X: x, Y: y},
 		Alcance:     alcance,
 		PlantedTime: time.Now(),
-		ExplodeTime: 3 * time.Second,
+		ExplodeTime: EXPLODE_TIME * time.Second,
 		Owner:       &owner,
 	}
 }
